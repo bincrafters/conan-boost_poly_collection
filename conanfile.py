39c4c30f1797a62513ea8e0602513ced5ae52580
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:28:55 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,26 @@ class BoostPoly_CollectionConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/poly_collection"
-    description = "For a description of this library, please visit http://boost.org/poly_collection "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "poly_collection"
+    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.Iterator/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Erasure/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing"
+
+                      #assert1 config0 core2 iterator5 mpl5 type_erasure6 type_traits3
 
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
