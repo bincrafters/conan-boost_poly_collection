8639765f2f94e0be0c9092e8168a7a3eec7f64ce
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:07 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,21 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostPoly_CollectionConan(base.BoostBaseConan):
     name = "boost_poly_collection"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_poly_collection"
-    lib_short_names = ["poly_collection"]
-    header_only_libs = ["poly_collection"]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_core",
-        "boost_iterator",
-        "boost_mp11",
-        "boost_mpl",
-        "boost_type_erasure",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
